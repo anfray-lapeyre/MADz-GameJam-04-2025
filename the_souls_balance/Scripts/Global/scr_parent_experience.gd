fe84ecaec7ee85802cdaea3983cfa00a79ce59de
Max <maxhumbert8899@yahoo.fr>
2025-04-15 07:48:33 +0200

    Fixed physics on exp

--- the_souls_balance/Scripts/Global/scr_parent_experience.gd
+++ the_souls_balance/Scripts/Global/scr_parent_experience.gd
@@ -1,27 +1,37 @@
 extends RigidBody2D
 
-@export var ex_fall_speed: float = 100.0 #falling speed of the experience
-@export var ex_horizontal_distance: float = 50 #How much movement from left to right the piece goes when input is pressed
-@export_range(0.0,1.0) var ex_smoothiness_of_transition:=0.5  #Between 0 and 1. 1 no smooth, 0 too smooth
-@export var ex_gravity: float = 300.0 #gravity when player doesn't control the experience anymore
+@export var ex_fall_speed: float = 100.0 # Falling speed when player is in control
+@export var ex_horizontal_distance: float = 50 # horizontal distance when pressing Q and D
+@export_range(0.0, 1.0) var ex_smoothiness_of_transition := 0.5 # Smoothness of the horizontal movement
+@export var ex_gravity: float = 1.0 # How strong is the gravity when you don't controle the experience anymore
 
 var target_position: Vector2
-var is_controlled: bool = true #Var to allow the player to move the experience and activate gravity
+var is_controlled: bool = true
 
-func _ready() -> void: #deactivate gravity when launched
+func _ready() -> void:
 	target_position = global_position
 	gravity_scale = 0.0
 	freeze = true
 
-func _process(delta: float) -> void: #input fonction
+func _physics_process(delta: float) -> void:
 	if is_controlled:
+		# horizontal deplacement on input pressed
 		if Input.is_action_just_pressed("move_left"):
 			target_position.x -= ex_horizontal_distance
 		elif Input.is_action_just_pressed("move_right"):
 			target_position.x += ex_horizontal_distance
+		# Makes the horizontal deplacement smooth
+		global_position.x = lerp(global_position.x, target_position.x, ex_smoothiness_of_transition)
+		# allows the free vertical fall
+		global_position.y += ex_fall_speed * delta
 
-		global_position.x = lerp(global_position.x, target_position.x, ex_smoothiness_of_transition) #Horizontal movement smooth
-		global_position.y += ex_fall_speed * delta #Constant fall
+#Need to separate in two functions because Godot doesn't like to change things while changing physics.
+func _on_area_2d_body_entered(body: Node) -> void:
+	if is_controlled:
+		is_controlled = false
+		call_deferred("_release_control") #To avoid everything happening all at once and yeeting the experiences
 
-func _set_control_to_false(): #Makes the player loose control of the experience
-	pass
+func _release_control(): #DJ makes me lose control
+	freeze = false
+	gravity_scale = ex_gravity
+	print ("release control")

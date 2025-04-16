4dcf6bb13b486b38a0bab16869f8deb78ffa6793
Max <maxhumbert8899@yahoo.fr>
2025-04-16 17:06:24 +0200

    Continued spawner, not done yet

--- the_souls_balance/Scripts/Resources/res_game_level.gd
+++ the_souls_balance/Scripts/Resources/res_game_level.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-@export var ex_experience_list: Array[PackedScene]
+var level_size : int = 0 #To makes sure it spawns the right number of pieces (changes in ready function later)
+@export var ex_experience_list: Array[PackedScene] #List of experience to spawn (keeps the order)
+@export var ex_spawn_position = Vector2(300,-100)
+@export var spawn_delay: float = 2.0
 
-# Called when the node enters the scene tree for the first time.
+
+
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	level_size = ex_experience_list.size() #Get the right array size
+	for i in range(level_size) : 
+		var spawn = ex_experience_list[i].instantiate()
+		add_child(spawn)
